7417063ff0a58b327319442750a5ec54e1e09342
lexggwp <alexeyfilipp@gmail.com>
2022-12-06 16:07:00 +0300

    added: class Component Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,44 +5,87 @@ import axios from 'axios';
 import avatar from '../../../src/assets/images/noavatar.jpg'
 
 
+class Users extends React.Component<UsersPropsType> {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
 
-const Users = (props: UsersPropsType) => {
-
-   !props.users.length && axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-       props.setUsers(response.data.items)
-   })
-
-
-    return (
-        <div>
-            {props.users.map(el => {
-                return (
-                    <div className={s.container} key={el.id}>
-                        <div className={s.leftBlock}>
-                            <div className={s.userPhotoBlock}>
-                                <img className={s.userPhoto} src={el.photos.small ? el.photos.small : avatar } alt=""/>
+    render() {
+        return (
+            <div>
+                {this.props.users.map((el) => {
+                    return (
+                        <div className={s.container} key={el.id}>
+                            <div className={s.leftBlock}>
+                                <div className={s.userPhotoBlock}>
+                                    <img className={s.userPhoto} src={el.photos.small ? el.photos.small : avatar}
+                                         alt=""/>
+                                </div>
+                                <div className={s.followBlock}>
+                                    {el.followed ?
+                                        <button onClick={() => this.props.toggleFollowMode(el.id)}>Follow</button> :
+                                        <button onClick={() => this.props.toggleFollowMode(el.id)}>Unfollow</button>
+                                    }
+                                </div>
                             </div>
-                            <div className={s.followBlock}>
-                                {el.followed ? <button onClick={ () => props.toggleFollowMode(el.id)}>Follow</button> :
-                                    <button onClick={ () => props.toggleFollowMode(el.id)}>Unfollow</button>
-                                }
+                            <div className={s.rightBlock}>
+                                <div className={s.mainText}>
+                                    <div className={s.fullName}>{el.name}</div>
+                                    <div className={s.status}>{el.status}</div>
+                                </div>
+                                <div className={s.location}>
+                                    <div className={s.city}>{'el.location.city'}</div>
+                                    <div className={s.country}>{'el.location.country'}</div>
+                                </div>
                             </div>
                         </div>
-                        <div className={s.rightBlock}>
-                            <div className={s.mainText}>
-                                <div className={s.fullName}>{el.name}</div>
-                                <div className={s.status}>{el.status}</div>
-                            </div>
-                            <div className={s.location}>
-                                <div className={s.city}>{'el.location.city'}</div>
-                                <div className={s.country}>{'el.location.country'}</div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    );
-};
+                    )
+                })}
+            </div>
+        );
+    }
+
+}
+
+// const Users = (props: UsersPropsType) => {
+//
+//    !props.users.length && axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//        props.setUsers(response.data.items)
+//    })
+//
+//
+//     return (
+//         <div>
+//             {props.users.map(el => {
+//                 return (
+//                     <div className={s.container} key={el.id}>
+//                         <div className={s.leftBlock}>
+//                             <div className={s.userPhotoBlock}>
+//                                 <img className={s.userPhoto} src={el.photos.small ? el.photos.small : avatar } alt=""/>
+//                             </div>
+//                             <div className={s.followBlock}>
+//                                 {el.followed ? <button onClick={ () => props.toggleFollowMode(el.id)}>Follow</button> :
+//                                     <button onClick={ () => props.toggleFollowMode(el.id)}>Unfollow</button>
+//                                 }
+//                             </div>
+//                         </div>
+//                         <div className={s.rightBlock}>
+//                             <div className={s.mainText}>
+//                                 <div className={s.fullName}>{el.name}</div>
+//                                 <div className={s.status}>{el.status}</div>
+//                             </div>
+//                             <div className={s.location}>
+//                                 <div className={s.city}>{'el.location.city'}</div>
+//                                 <div className={s.country}>{'el.location.country'}</div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     );
+// };
 
 export default Users;
